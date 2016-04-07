fd8c7fac092ad753a3739d27487998e6a92fd93b
COLIN <reckocp@gmail.com>
2016-04-07 15:52:38 -0500

    Fixed models and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
- resources: user
- resources: post
+ resources :user
+ resources :post
 
   namespace :api do
    resources :posts
--- db/schema.rb
+++ db/schema.rb
@@ -18,7 +18,7 @@ ActiveRecord::Schema.define(version: 20160407201301) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
-    t.string   "body"
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
