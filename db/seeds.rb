776ef67bdf034c941dbf3d59730b3b5433afa4c7
COLIN <reckocp@gmail.com>
2016-04-07 15:19:51 -0500

    Models and seed completed

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+
+(1..20).each do |num|
+ post = Post.create!(title: Faker::Hipster.word,
+                    body: Faker::Hipster.paragraph)
+end
+
+10.times do
+  user = User.create!(name: Faker::Superhero.name)
+end
